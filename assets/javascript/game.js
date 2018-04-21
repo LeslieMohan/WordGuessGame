c6e12869cc3de9428d80b076b411a9de330dfde9
Leslie Mohan <lesliemohan@Leslies-MacBook-Air.local>
2018-04-21 11:49:41 -0400

    adding first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+/*pick a random word*/
+var songTitles = ["back in black", "kasmhir", "stairway to heaven",
+"sweet emotion", "comfortably numb", "baba o'riley",
+"crazy  train", "hotel california", "imagine", "more than a feeling",
+"smoke on the water", "rocket man", "barracuda", "cocaine",
+"tom sawyer", "black dog", "free fallin", "free bird", "paranoid",
+"we will rock you", "enter sandman", "time", "money"];
+
+var randomword = songTitles []
+
+/*player takes a guess*/
+var guess = [];
+
+/*show player their progress*/
+
+
+/*ifthe players guess is a valid letter in word, then update players progresss*/
+
+
+
+/*keep track of missed letter choices*/
+
+
+/*show progress*/
+
+
+/* win and finish if player guesses word*/
+
+
