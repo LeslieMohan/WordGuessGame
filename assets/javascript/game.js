66ae9f29b0b53d9dace881663b8bd392ea74687d
Leslie Mohan <lesliemohan@Leslies-MacBook-Air.local>
2018-06-14 00:43:53 -0400

    updated version with images working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,12 +8,21 @@ var wrongWord = [];
 var underScore = [];
 var wins = 0;
 var loses = 0;
+const maxTries = 9;
+
 
 var wrongLetter = [];
 var guessesLeft = 9;
 
+
 document.getElementById('guessesLeft').textContent = guessesLeft;
 
+updateSnowmanImage();
+
+function updateSnowmanImage() {
+    var img = document.getElementById("theImage");
+    img.src = "assets/images/" + (maxTries - guessesLeft) + ".png";
+};
 
 
 //select word randomly at beginning of game
@@ -37,51 +46,60 @@ var makeUnderscores = () => {
 }
 console.log(makeUnderscores());
 
-function winLose() {
-    
-    if (winCounter === randomWord.length)
-{
-        alert("You Win!")
-}
-    else if (guessesLeft === 0)
-{            
-    alert("You Lost!")
-}
-}
+
 //user input/guesses
 document.onkeyup = function(event) {
-    event.preventDefault();
-    
-    var userGuess = event.key;
-    console.log(userGuess);
+        event.preventDefault();
+        if (!(event.which <= 90 && event.which >= 65)) return
+        var userGuess = event.key.toLowerCase();
+        console.log(userGuess);
 
-    if(randomWord.indexOf(userGuess) > -1) {
+        if(randomWord.indexOf(userGuess) > -1) {
 
 //check if user's guess is correct
-//if correct, push to right array
+
+//if correct, push to right guesses array
         rightWord.push(userGuess);
         console.log(rightWord);
+
 //put letter in place of underscore
         underScore[randomWord.indexOf(userGuess)] = userGuess;
         docUnderScore[0].innerHTML = underScore.join('');
         docRightGuess[0].innerHTML = rightWord;
-        if(underScore.join('') == randomWord) {
-            alert('You Win!');
-            winCounter++;
-            winLose();
+
+        if(underScore.join('') === randomWord) {
+
+            wins++;
+            $("#result").text("YOU WIN!");
+            $("#wins").text(wins);
+            
+            
+            console.log("winner");
         }
     }
     else {
 
-//if wrong, push to wrong array
+//if wrong, push to wrong guesses array
         wrongWord.push(userGuess);
         docWrongGuess[0].innerHTML = wrongWord;
-        console.log(wrongWord);
+        guessesLeft--;
+        $('#guessesLeft').text(guessesLeft);
+        updateSnowmanImage();
     }
+   
+    if(guessesLeft <= 0) { 
+        loses++;
+    $("#result").text("Try Again");
+    $("#loses").text(loses);
 
+
+    console.log("That's wrong");
+    
 
 };
 
+
+};
 
 });
 
